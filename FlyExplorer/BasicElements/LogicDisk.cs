e42e10bf778b3e7c05b1b1d9abdf57c525748eb9
Artem <zhimovskij@yandex.ru>
2017-12-17 22:56:01 +0500

    Add structure project v2.1

--- FlyExplorer/BasicElements/LogicDisk.cs
+++ FlyExplorer/BasicElements/LogicDisk.cs
@@ -1,6 +1,24 @@
-﻿namespace FlyExplorer.BasicElements
+﻿using System;
+
+namespace FlyExplorer.BasicElements
 {
     internal class LogicDisk
     {
+        public string NameDisk { get; set; }
+        public ulong Size { get; set; }
+        public ulong FreeVolume { get; set; }
+        public ulong OccupiedVolume { get; set; }
+        public string LabelDisk { get; set; }
+
+
+        public LogicDisk()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
     }
 }
