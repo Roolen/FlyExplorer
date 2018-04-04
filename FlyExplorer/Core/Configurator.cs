1333c6a52002cb039c919584a54d5335bbbcea70
Artem <zhimovskij@yandex.ru>
2018-04-04 22:34:09 +0500

    Dev: make method for writing in registry (#7)

--- FlyExplorer/Core/Configurator.cs
+++ FlyExplorer/Core/Configurator.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace FlyExplorer.Core
 {
     static class Configurator
     {
+        static void WriteFavoriteInRegistry(string keyName, object value)
+        {
+            try
+            {
+                RegistryKey keyForRegistry = Registry.CurrentUser;
+
+                RegistryKey subKey = keyForRegistry.CreateSubKey("HKEY_CURRENT_USER\\Software\\FlyExplorer");
+
+                subKey.SetValue(keyName, value);
+            }
+            catch (Exception e)
+            {
+
+                Log.Write($"{e} Writing registry {keyName}");
+            }
+        }
     }
 }
