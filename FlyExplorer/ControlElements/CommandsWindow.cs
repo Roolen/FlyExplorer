732f789a9343509882795e62d002313ae636329b
Artem <zhimovskij@yandex.ru>
2018-04-09 01:37:29 +0500

    Dev: now with a lack of access rights, a message appears (#17)

---
+++ FlyExplorer/ControlElements/CommandsWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlyExplorer.ControlElements
+{
+    class CommandsWindow
+    {
+        public static RoutedCommand CloseTab { get; set; }
+
+        static CommandsWindow()
+        {
+            CloseTab = new RoutedCommand("CloseTab", typeof(MainWindow));
+        }
+    }
+}
